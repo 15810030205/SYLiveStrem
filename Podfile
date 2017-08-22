13e0f4d042db8ebb44bd977284f748197099aeaa
haosongyan <haosongyan@jiuai.mobi>
2017-08-22 18:24:56 +0800

    增加项目

---
+++ Podfile
@@ -0,0 +1,9 @@
+
+platform :ios, '8.0'
+use_frameworks!
+
+target 'SYLiveStrem' do
+pod 'Just'
+pod 'Kingfisher'
+
+end
---
+++ Pods/Target Support Files/Pods-SYLiveStrem/Pods-SYLiveStrem-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SYLiveStrem : NSObject
+@end
+@implementation PodsDummy_Pods_SYLiveStrem
+@end
